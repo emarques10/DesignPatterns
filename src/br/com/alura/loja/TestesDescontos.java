2e304a0ec8f845a44de9ae40cc5a0fd76e7ef101
ewmarque <ewerton.marques@embraer.com.br>
2021-08-03 14:56:42 -0300

    Calculando descontos

---
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,23 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesDescontos {
+
+	public static void main(String[] args) {
+		Orcamento primeiro = new Orcamento(new BigDecimal("100"),4);
+		Orcamento segundo = new Orcamento(new BigDecimal("1000"),1);
+
+		
+		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+		System.out.println(calculadora.calcular(primeiro));
+		System.out.println(calculadora.calcular(segundo));
+
+	}
+
+}
